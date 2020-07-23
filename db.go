aafcebefeb329993b0b27a359e8e03c0481c90cd
man-inblack <just_yang@yeah.net>
2020-07-23 15:07:51 +0800

    del comment

--- db.go
+++ db.go
@@ -9,14 +9,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// var url string = dbConfig.dbUrl "mongodb://blogadmin:xEN2vyrnZQDcvnaMcZW3@182.92.236.123"
-// var blogDB string = "test_db"
-// var blogTextCol string = "blog_text"
-
-// var url string = Config.DB.Server
-// var blogDB string = Config.DB.Blog_db
-// var blogTextCol string = Config.DB.Blog_text_col
-
 func connectDb() (*mongo.Client, context.Context) {
 	log.Println(Config.DB.Server, Config.DB.Blog_db, Config.DB.Blog_text_col, "done!")
 	client, err := mongo.NewClient(options.Client().ApplyURI(Config.DB.Server))
